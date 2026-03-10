044e846c43f13787982834ae3c28dfe031bec7ab
LeoKliemann <lgkliemann05@gmail.com>
2026-03-10 02:45:11 -0400

    Changed counting start to 0

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ public class Main{
     {
         // 1 to 30
         System.out.println("Count up to 30: ");
-        for (int countUp = 1; countUp <= 30; countUp++)
+        for (int countUp = 0; countUp <= 30; countUp++)
         {
             System.out.print(countUp + " ");
         }
         // 30 to 1
         System.out.println(" ");
         System.out.println("Count down from 30:   ");
-        for (int countDown = 30; countDown >= 1; countDown--)
+        for (int countDown = 30; countDown >= 0; countDown--)
         {
             System.out.print(countDown + " ");
         }
