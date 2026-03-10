4cca6d570c61accf0097028dc7e03c0ddcefdeb5
LeoKliemann <lgkliemann05@gmail.com>
2026-03-10 03:46:28 -0400

    Finished Part A

---
+++ src/PartA.java
@@ -0,0 +1,35 @@
+public class PartA {
+    public static void main(String[] args) {
+
+        // 1 to 30
+        System.out.println("Count up to 30: ");
+        for (int countUp = 0; countUp <= 30; countUp++) {
+            System.out.print(countUp + " ");
+        }
+        System.out.println();
+
+        // 30 to 1
+        System.out.println(" ");
+        System.out.println("Count down from 30:   ");
+        for (int countDown = 30; countDown >= 0; countDown--) {
+            System.out.print(countDown + " ");
+        }
+        System.out.println();
+
+        // Count by 3 from 0 to 18
+        System.out.println(" ");
+        System.out.println("Count up by 3 from 0 to 18: ");
+        for (int countByThree = 0; countByThree <= 18; countByThree += 3) {
+            System.out.print(countByThree + " ");
+        }
+        System.out.println();
+
+        // Count down by 2 from 10 to 0
+        System.out.println(" ");
+        System.out.println("Count down by 2 from 10 to 0: ");
+        for (int countDownByTwo = 10; countDownByTwo >= 0; countDownByTwo -= 2) {
+            System.out.print(countDownByTwo + " ");
+        }
+        System.out.println();
+    }
+}
